9ed93bcf63f12a77d08d56978413e6ea69a2bc48
mjarrett2019 <matt.jarrett.ext@louisvuitton.com>
2022-08-25 07:47:32 -0400

    Added comments and docstrings Added choice to not select pronouns in selection

--- main.py
+++ main.py
@@ -17,28 +17,49 @@ class Location:
 
 @app.route('/signature <email> <pronouns>', methods=['GET', 'POST'])
 def signature(email, pronouns):
+    """Generates email signature with information from LV Databases"""
+
+    # Create a connection to SQL
     con = sqlite3.connect('lvdatabase.db')
+    
+    # Create cursor object
     cur = con.cursor()
+    
+    # SQL query to search for user by email
     query = f"""SELECT * from Users
                 WHERE email = {email}"""
     cur = cur.execute(query)
     data = cur.fetchall()
+    
+    # Create User class instance
     user = User(name=data['Name'], job_title=data['Job Title'], phone=data['Phone'])
-    location = Location(name=data['Location'], address=['Address'])
-    signature = f"<b>{user.name}</b> | {pronouns} | {user.job_title} | {location.name}\n | {location.address} | Mobile: {user.phone} | {email}"
+    
+    # Create Location instance
+    location = Location(name=data['Location'], address=data['Address'])
+
+    # Generates signature string based on if the user chose to select pronouns
+    if pronouns == 'I Do Not Wish To Select':
+        signature = signature = f"<b>{user.name}</b> | {user.job_title} | {location.name}\n | {location.address} | Mobile: {user.phone} | {email}"
+    else:
+        signature = f"<b>{user.name}</b> | {pronouns} | {user.job_title} | {location.name}\n | {location.address} | Mobile: {user.phone} | {email}"
+    
+    # Returns the signature string to the HTML document to be rendered to the client
     return render_template('signature.html', signature=signature)
 
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    """Gets the email and pronouns (if the user chooses) via HTML form"""
     if request.method == "POST":
         email = request.form['email']
         pronouns = request.form['pronouns']
         
+        # Returns the email and pronouns to pass it to the /signature route
         return redirect(url_for('signature', email=email, pronouns=pronouns))
 
     else:
+        # Gets the email and pronouns from the user
         email = request.args.get('email')
         pronouns = request.args.get('pronouns')
 
